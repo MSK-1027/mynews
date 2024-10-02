368e7eaa3f36911713b706c19ba7bc7963cdce2d
masaki <jmtdsiteyo1027@outlook.jp>
2024-10-02 11:07:01 +0000

    PHP/Laravel 161719

--- resources/views/admin/profile/index.blade.php
+++ resources/views/admin/profile/index.blade.php
@@ -1,54 +1,41 @@
 @extends('layouts.admin')
-@section('title', '登録済みニュースの一覧')
-
+@section('title', '登録済プロフィールの一覧')
+<!--  -->
 @section('content')
     <div class="container">
         <div class="row">
-            <h2>プロフィール</h2>
+            <h2>プロフィール一覧</h2>
         </div>
         <div class="row">
             <div class="col-md-4">
-                <a href="{{ route('admin.profile.add') }}" role="button" class="btn btn-primary">新規作成</a>
+                <a href="{{ route('admin.profie.add') }}" role="button" class="btn btn-primary">新規作成</a>
             </div>
-            <div class="col-md-8">
-                <form action="{{ route('admin.profile.index') }}" method="get">
-                    <div class="form-group row">
-                        <label class="col-md-2">タイトル</label>
-                        <div class="col-md-8">
-                            <input type="text" class="form-control" name="cond_title" value="{{ $cond_title }}">
-                        </div>
-                        <div class="col-md-2">
-                            @csrf
-                            <input type="submit" class="btn btn-primary" value="検索">
-                        </div>
-                    </div>
-                </form>
-            </div>
+            
         </div>
         <div class="row">
-            <div class="list-profile col-md-12 mx-auto">
+            <div class="list-profie col-md-12 mx-auto">
                 <div class="row">
                     <table class="table table-dark">
                         <thead>
                             <tr>
-                                <th width="10%">ID</th>
-                                <th width="20%">タイトル</th>
-                                <th width="50%">本文</th>
-                                <th width="10%">操作</th>
+                                <th width="10%">名前</th>
+                                <th width="10%">性別</th>
+                                <th width="10%">趣味</th>
+                                <th width="50%">自己紹介欄</th>
                             </tr>
                         </thead>
                         <tbody>
-                            @foreach($posts as $profile)
+                            @foreach($posts as $profie)
                                 <tr>
-                                    <th>{{ $profile->id }}</th>
-                                    <td>{{ Str::limit($profile->title, 100) }}</td>
-                                    <td>{{ Str::limit($profile->body, 250) }}</td>
+                                    <th>{{ $profie->id }}</th>
+                                    <td>{{ Str::limit($profie->title, 100) }}</td>
+                                    <td>{{ Str::limit($profie->body, 250) }}</td>
                                     <td>
                                         <div>
-                                            <a href="{{ route('admin.profile.edit', ['id' => $profile->id]) }}">編集</a>
+                                            <a href="{{ route('admin.profie.edit', ['id' => $profie->id]) }}">編集</a>
                                         </div>
                                         <div>
-                                            <a href="{{ route('admin.profile.delete', ['id' => $profile->id]) }}">削除</a>
+                                            <a href="{{ route('admin.news.delete', ['id' => $profie->id]) }}">削除</a>
                                         </div>
                                     </td>
                                 </tr>
